f586ea6263b4b84621ce3a461bc6fea90a7a6fad
dat <dat@dats-MacBook-Pro.local>
2015-07-30 23:05:38 +0700

    Initial Commit

---
+++ iweather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iweather
+//
+//  Created by dat on 7/30/15.
+//  Copyright (c) 2015 dat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iweather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iweather
+//
+//  Created by dat on 7/30/15.
+//  Copyright (c) 2015 dat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
